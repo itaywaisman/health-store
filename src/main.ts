066c94f5e03771fcb9f7beb8a2ee9cc7f1c38753
Itay Waisman <itay.waisman@gmail.com>
2020-04-19 19:23:00 +0300

    adding security packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(csurf());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 500, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
