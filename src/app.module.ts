3b849e3d5f5292d63dd71ce8fab953aaf9dc0ad5
Itay Waisman <itay.waisman@gmail.com>
2020-04-19 19:39:58 +0300

    removing unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { HealthEntriesModule } from './healthEntries/healthEntries.module';
 import { BusinessModule } from './business/business.module';
@@ -10,7 +8,5 @@ import { BusinessModule } from './business/business.module';
   imports: [MongooseModule.forRoot('mongodb://localhost/nest'),
     BusinessModule,
     HealthEntriesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
