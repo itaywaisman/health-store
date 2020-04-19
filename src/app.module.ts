3fcead49daecc97f2702bca3f72e6dde407dab38
Itay Waisman <itay.waisman@gmail.com>
2020-04-19 19:37:33 +0300

    adding the modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { HealthEntriesModule } from './healthEntries/healthEntries.module';
+import { BusinessModule } from './business/business.module';
 
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'),
+    BusinessModule,
+    HealthEntriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
